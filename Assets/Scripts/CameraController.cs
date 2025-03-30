a063bddaf89f5612b6bf6096b1ffc05ef6743571
MakBongo <35739073+MakBongo@users.noreply.github.com>
2025-03-30 19:05:14 +0800

    v0.6.2
    Change player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,33 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    /*
-    public float followAhead = 5f;
-    private GameObject player;
-    private Vector3 targetPosition;
-    public float smoothing = 5f;
-    private void Start()
-    {
-        player = GameObject.Find("Player");
-    }
-
-
-    void Update()
-    {
-        if (player.transform.localScale.x > 0f)
-        {
-            targetPosition = new Vector3(player.transform.position.x + followAhead, transform.position.y, transform.position.z);
-        }
-        else
-        {
-            targetPosition = new Vector3(player.transform.position.x - followAhead, transform.position.y, transform.position.z);
-        }
-
-        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
-
-    }
-    */
-
     public Transform target;
     public float smoothing;
 
